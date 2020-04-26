bdcc6c5e6e84924c1332e1880677ed17eae8ad64
stewartmax <stewartmax100@gmail.com>
2020-04-26 19:44:51 +1200

    Implemented a PUT request

--- src/Supermarket.API/Persistance/Repositories/CategoryRepository.cs
+++ src/Supermarket.API/Persistance/Repositories/CategoryRepository.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Repositories;
 using Supermarket.API.Persistance.Contexts;
+using Supermarket.API.Domain.Services.Communication;
 
 namespace Supermarket.API.Persistance.Repositories
 {
@@ -22,6 +23,16 @@ namespace Supermarket.API.Persistance.Repositories
         public async Task AddAsync(Category category)
         {
             await _context.Categories.AddAsync(category);
+        }
+
+        public async Task<Category> FindByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
         }
     }
 }
