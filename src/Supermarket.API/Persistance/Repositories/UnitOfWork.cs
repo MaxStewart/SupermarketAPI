3bb2f4fee07c90c42c96fe65123b9542a068175a
stewartmax <stewartmax100@gmail.com>
2020-04-26 18:20:24 +1200

    Finished post for Categories (with validation)

---
+++ src/Supermarket.API/Persistance/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Persistance.Contexts;
+
+namespace Supermarket.API.Persistance.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;     
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- src/Supermarket.API/Startup.cs
+++ src/Supermarket.API/Startup.cs
@@ -35,6 +35,7 @@ namespace Supermarket.API
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
