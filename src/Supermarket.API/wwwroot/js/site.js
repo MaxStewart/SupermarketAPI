eeb4922d6825f250e9bdeb067a4887f47f78f65c
stewartmax <stewartmax100@gmail.com>
2020-05-10 15:36:24 +1200

    Added a template for the form to add new products

--- src/Supermarket.API/wwwroot/js/site.js
+++ src/Supermarket.API/wwwroot/js/site.js
@@ -3,7 +3,31 @@ const CategoriesURI = 'api/categories';
 
 function postItems()
 {
+    console.log("ITEM POSTED");
+}
 
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
 }
 
 function getCategories()
