8571fdd6a47f1d7c94497e0c522b55cac45666a5
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-07-06 22:14:27 +1200

    added cookies and a listener to the add item to cart button

--- src/Supermarket.API/wwwroot/js/index.js
+++ src/Supermarket.API/wwwroot/js/index.js
@@ -100,6 +100,17 @@ function createItemList(data, category)
             addToCartButton.style.right = "0";
             addToCartButton.style.margin = "5px";
 
+            addToCartButton.addEventListener("click", function () {
+                var itemToAdd = {
+                    name: item.name,
+                    description: item.description,
+                    price: item.price
+                };
+
+                addItemToCart(itemToAdd);
+
+            });
+
 
             cardBody.appendChild(cardTitle);
             cardBody.appendChild(cardPrice);
@@ -121,5 +132,47 @@ function createItemList(data, category)
         noItemTextLine.classList += "mx-auto ";
         noItemTextLine.classList += "font-weight-bolder";
         itemRow.appendChild(noItemTextLine);
+    }
+}
+
+function addItemToCart(item) {
+
+    console.log(item.name);
+    console.log(item.price);
+
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        alert("Welcome again " + username);
+    } else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
     }
 }
