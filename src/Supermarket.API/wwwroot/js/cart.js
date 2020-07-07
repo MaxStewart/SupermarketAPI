01a186a9e185682f891c22c666c46b0b67e7bd1b
Max Stewart <39015672+MaxStewart@users.noreply.github.com>
2020-07-07 17:58:06 +1200

    started adding dynamic items to cart table in checkout page

--- src/Supermarket.API/wwwroot/js/cart.js
+++ src/Supermarket.API/wwwroot/js/cart.js
@@ -1,1 +1,38 @@
-﻿
+﻿function getCartItems(sessionStorageKey) {
+
+    if (sessionStorage.getItem(sessionStorageKey)) {
+        // Get cart contents
+        let cartContents = JSON.parse(sessionStorage.getItem(sessionStorageKey));
+
+        let tbody = document.createElement("tbody");
+
+        let tr = document.createElement("tr");
+
+        let td1 = document.createElement("td");
+
+        let td2 = document.createElement("td");
+
+
+        for (let i = 0; i < cartContents.length; i++) {
+            console.log(cartContents[i]);
+        }
+    }
+
+}
+
+/*
+<tbody>
+    <tr>
+        <td><p style="margin-bottom:0px;">Item</p><p style="margin-bottom:0px;" class="font-weight-light">Description</p></td>
+        <td class="text-right">$25</td>
+    </tr>
+    <tr class="table-success">
+        <td>Promo Code</td>
+        <td class="text-right">-$20</td>
+    </tr>
+    <tr>
+        <td class="font-weight-bold">Total</td>
+        <td class="text-right font-weight-bolder">$20</td>
+    </tr>
+</tbody>
+*/
